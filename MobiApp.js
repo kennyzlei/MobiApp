7ecbc5795c63ddfd65578e5ed14271e2fed4665e
Arthur Del Esposte <arthurmde@gmail.com>
2016-01-13 16:26:24 -0200

    Add issue form submission

--- MobiApp.js
+++ MobiApp.js
@@ -1,17 +1,28 @@
+Issues = new Mongo.Collection('issues');
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.newIssue.helpers({
+    'issue': function(){
+      return "Some other text"
+    },
+    'other': function(){
+      return "Try"
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.newIssue.events({
+    'submit form': function(){
+      var title = event.target.title.value;
+      var description = event.target.description.value;
+      console.log(title, description)
+      Issues.insert({
+        title: title,
+        description: description,
+        status: 'open'
+      });
     }
   });
 }
