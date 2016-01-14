3d52bc91b3fae17c91fec82c48ee61f01b461982
oskarpolski <kocol@college.harvard.edu>
2016-01-14 10:44:18 -0200

    Addet universal layout in MobiApp.js and MobiApp.html. Ask Oskar if any questions.

--- MobiApp.js
+++ MobiApp.js
@@ -4,13 +4,31 @@ Issues = new Mongo.Collection('issues');
 // Routing
 
 Router.route('/', function () {
-  // render the Home template with a custom data context
+  // render the Home template with newIssue submission
+  this.layout('ApplicationLayout');
   this.render('newIssue');
+  this.render('Footer', {to: 'footer'});
 });
 
+// navigate to About
+Router.route('/about', function () {
+  this.layout('ApplicationLayout');
+  this.render('About');
+  this.render('Footer', {to: 'footer'});  
+});
+
+//navigate to submited issues
+Router.route('/issues-list', function () {
+  this.layout('ApplicationLayout');
+  this.render('IssuesList');
+  this.render('Footer', {to: 'footer'}); 
+});
+
+
+//-----------------------------------------TO BE DELETED: WAITS FOR REVISION ------------------------------------------
 // when you navigate to "/one" automatically render the template named "One".
 Router.route('/upload');
 
 // when you navigate to "/login" automatically render the template named "Login".
 Router.route('/newIssue');
-
+//----------------------------------------------------------------------------------------------------------------------
