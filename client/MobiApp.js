b125db55046342412d2e8e6837da18a7fcb3662b
oskarpolski <kocol@college.harvard.edu>
2016-01-15 14:02:41 -0200

    All the entries from the form are saved in the database now

--- client/MobiApp.js
+++ client/MobiApp.js
@@ -6,22 +6,38 @@ Template.newIssue.events({
     var title = event.target.title.value;
     var description = event.target.description.value;
     var imageURL = Session.get('imageURL');
+    var updateStatus = event.target.updcheck.checked;
     console.log(title, description);
-    if (title && description && Geolocation.latLng()) {
+    if (title && description) {
         Issues.insert({
             title: title,
             description: description,
-            status: 'open',
+            status: 'pending',
             lat: Geolocation.latLng().lat,
             lng: Geolocation.latLng().lng,
             userID: Meteor.userId(),
             imageURL: imageURL,
             createdAt: new Date(),
-            lastModified: new Date()
+            lastModified: new Date(),
+            updates: updateStatus
+
         });
         Router.go('/issues-list');
     } else {
         console.log("form not valid");
+    }
+  }
+});
+
+Template.newIssue.helpers({
+  showMap: function(){
+    //here check if GPS data is available
+    //add html to show good map
+    if (Geolocation.latLng()){
+      return true;
+    }
+    else{
+      return false;
     }
   }
 });
