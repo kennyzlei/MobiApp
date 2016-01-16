7456693fafd57264597890c341492e7e42d821c2
oskarpolski <kocol@college.harvard.edu>
2016-01-16 09:33:44 -0200

    Maps do not work on iPhone and coordinates of a marker need to be obtained

--- client/MobiApp.js
+++ client/MobiApp.js
@@ -1,5 +1,13 @@
 Geolocation.latLng()
 
+Meteor.startup(function() {
+  GoogleMaps.load({
+    key: 'AIzaSyCF_82_4WnRrrTXUP6Aq9HIPweoOaTroEc',
+    libraries: 'places'  // also accepts an array if you need more than one
+  });
+  console.log("GoogleMaps are set up");
+});
+
 Template.newIssue.events({
   'submit form': function(){
     event.preventDefault();
@@ -8,13 +16,13 @@ Template.newIssue.events({
     var imageURL = Session.get('imageURL');
     var updateStatus = event.target.updcheck.checked;
     console.log(title, description);
-    if (title && description) {
+    if (title) {
         Issues.insert({
             title: title,
             description: description,
             status: 'pending',
-            lat: Geolocation.latLng().lat,
-            lng: Geolocation.latLng().lng,
+            //lat: Geolocation.latLng().lat,
+            //lng: Geolocation.latLng().lng,
             userID: Meteor.userId(),
             imageURL: imageURL,
             createdAt: new Date(),
@@ -26,8 +34,44 @@ Template.newIssue.events({
     } else {
         console.log("form not valid");
     }
-  }
+  },
+  
 });
+
+Template.newIssue.onRendered(function () {
+    this.autorun(() => {
+      // Wait for API to be loaded
+      if (GoogleMaps.loaded()) {
+        $('#location-description')
+        .geocomplete({
+          map: $("#map"),
+          mapOptions: {
+            zoom: 13
+          },
+          markerOptions: {
+            draggable: true
+            //Bounds that need to be implemented
+        //-23.798107,-46.886902
+        //-23.366408,-46.299133
+          },
+          location:[-23.5500, -46.6333]
+        })
+
+        .bind("geocode:result", function(event, result){
+          console.log(result);
+          var markLat = result.geometry.location.lat;
+          var markLon = result.geometry.location.lon;
+        })
+        .bind("geocode:error", function (event, status) {
+          console.log(status);
+        })
+        .bind("geocode:multiple", function (event, results) {
+          console.log("Multiple: " + results.length + " results found");
+        });
+      }
+    });
+
+  });
 
 Template.newIssue.helpers({
   showMap: function(){
